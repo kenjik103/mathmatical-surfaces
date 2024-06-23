021c752dbb977af332c2a3537c09b15e4f2372f9
Kenjiro Kanagy <kenjik103@gmail.com>
2024-06-23 15:25:13 -0700

    graphs are 3d now

--- ComplexGraphs/Assets/Scripts/FunctionLibrary.cs
+++ ComplexGraphs/Assets/Scripts/FunctionLibrary.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 using static UnityEngine.Mathf; 
 public static class FunctionLibrary
 {
-    public delegate float Function(float x, float t);
+    public delegate float Function(float x, float z, float t);
 
     public enum FunctionName
     {
@@ -15,18 +15,19 @@ public static class FunctionLibrary
         return functions[(int)name];
     }
     
-    public static float Wave(float x, float t) {
-        return Sin(Mathf.PI * (x + t));
+    public static float Wave(float x, float z, float t) {
+        return Sin(PI * (x + z + t));
     }
     
-    public static float MultiWave(float x, float t) {
-        float y = Sin(Mathf.PI * (x + 0.5f * t));
-        y += Sin(2f * PI * (x + t)) * 0.5f;
-        return y * (2f/3f); //scale function down to fit (-1,1) range 
+    public static float MultiWave(float x, float z, float t) {
+        float y = Sin(PI * (x + 0.5f * t));
+        y += Sin(2f * PI * (z + x + t)) * 0.5f;
+        y += Sin(PI * (x + z + 0.25f * t));
+        return y * (1f / 2.5f); //scale function down to fit (-1,1) range 
     }
 
-    public static float Ripple(float x, float t) {
-        float d = Abs(x);
+    public static float Ripple(float x, float z, float t) {
+        float d = Sqrt(x * x + z * z);
         float y = Sin( PI * (4f * d - t));
         return y / (1f + 10f * d);
     }
--- ComplexGraphs/Assets/Scripts/Graph.cs
+++ ComplexGraphs/Assets/Scripts/Graph.cs
@@ -21,10 +21,15 @@ public class Graph: MonoBehaviour
         float step = 2f / resolution;
         Vector3 scale = Vector3.one * step;
         Vector3 position = Vector3.zero;
-        points = new Transform[resolution];
-        for (int i = 0; i < points.Length; i++) {
+        points = new Transform[resolution * resolution];
+        for (int i = 0, x = 0, z = 0; i < points.Length; i++, x++) {
+            if (x == resolution) {
+                x = 0;
+                z += 1;
+            }
             Transform point = points[i] = Instantiate(pointPrefab, transform, false);
-            position.x = (i + 0.5f) * step - 1f;
+            position.x = (x + 0.5f) * step - 1f;
+            position.z = (z + 0.5f) * step - 1f;
             point.localPosition = position;
             point.localScale = scale;
         }
@@ -36,7 +41,7 @@ public class Graph: MonoBehaviour
         for (int i = 0; i < points.Length; i++) {
             Transform point = points[i];
             Vector3 position = point.localPosition;
-            position.y = f(position.x, time);
+            position.y = f(position.x, position.z, time);
             point.localPosition = position;
         }
     }
