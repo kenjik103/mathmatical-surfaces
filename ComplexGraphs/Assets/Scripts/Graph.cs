688d0beb419195a78b8062a8b7849044e7d7409c
Kenjiro Kanagy <kenjik103@gmail.com>
2024-06-23 16:29:59 -0700

    fin. Added more complex graphs (rotating sphere and torus)

--- ComplexGraphs/Assets/Scripts/Graph.cs
+++ ComplexGraphs/Assets/Scripts/Graph.cs
@@ -20,17 +20,9 @@ public class Graph: MonoBehaviour
     void Awake() {
         float step = 2f / resolution;
         Vector3 scale = Vector3.one * step;
-        Vector3 position = Vector3.zero;
         points = new Transform[resolution * resolution];
-        for (int i = 0, x = 0, z = 0; i < points.Length; i++, x++) {
-            if (x == resolution) {
-                x = 0;
-                z += 1;
-            }
+        for (int i = 0; i < points.Length; i++) {
             Transform point = points[i] = Instantiate(pointPrefab, transform, false);
-            position.x = (x + 0.5f) * step - 1f;
-            position.z = (z + 0.5f) * step - 1f;
-            point.localPosition = position;
             point.localScale = scale;
         }
     }
@@ -38,11 +30,16 @@ public class Graph: MonoBehaviour
     void Update() {
         FunctionLibrary.Function f = FunctionLibrary.GetFunction(function);
         float time = Time.time;
-        for (int i = 0; i < points.Length; i++) {
-            Transform point = points[i];
-            Vector3 position = point.localPosition;
-            position.y = f(position.x, position.z, time);
-            point.localPosition = position;
+        float step = 2f / resolution;
+        float v = 0.5f * step - 1f;
+        for (int i = 0, x = 0, z = 0; i < points.Length; i++, x++) {
+            if (x == resolution) {
+                x = 0;
+                z++;
+                v = (z + 0.5f) * step - 1f;
+            }
+            float u = (x + 0.5f) * step - 1f;
+            points[i].localPosition = f(u, v, time);
         }
     }
 }
